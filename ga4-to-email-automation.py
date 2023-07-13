d69534bb3b44db57177039846611ef73f2c8a0ab
Ebubechukwu Oguaju-Dike <83477062+ebube-tech@users.noreply.github.com>
2023-07-13 18:37:17 +0100

    Create ga4-to-email-automation.py

---
+++ ga4-to-email-automation.py
@@ -0,0 +1,38 @@
+import os
+import pandas as pd
+from jj_data_connector.ga4 import GA4Report, Metrics, Dimensions
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'sa_ga4_demo.json'
+property_id = '307310528'
+
+# creating GA4Report object instance
+ga4 = GA4Report(property_id)
+
+# variables
+dimension_list = ['date', 'browser']
+metric_list = ['totalUsers', 'bounceRate']
+date_range1 = ('2022-03-01', '2022-03-31')
+date_range2 = ('2022-04-01', '2022-04-30')
+
+# run the GA4 report
+report = ga4.run_report(
+    dimension_list, 
+    metric_list, 
+    date_ranges=[date_range1, date_range2],
+    row_limit=10,
+    offset_row=10
+)
+
+report['row_count']
+
+# report.keys()
+print(report['response'])
+
+report['headers']
+report['rows']
+df = pd.DataFrame(columns=report['headers'], data=report['rows'])
+print(df)
+
+# export to CSV and Excel files
+df.to_csv('demo1.csv', index=False)
+df.to_excel('demo1.xlsx', index=False)
